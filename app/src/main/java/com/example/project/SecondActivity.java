15a678cef8662cd512a181469b61db8157ff37cb
d22samma <d22samma@student.his.se>
2023-05-05 16:07:50 +0200

    Add Button and store Data Button play save function and start new activity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,45 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    // in My MainActivity class
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // Get a reference to the shared preference
+        myPreferenceRef = getSharedPreferences("key", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        // Display preferences
+        EditText prefTextRef = findViewById(R.id.editText);
+        prefTextRef.setText(myPreferenceRef.getString("key", "No preference found."));
+
+        Button button = findViewById(R.id.SaveButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save();
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    void save() {
+        EditText sharedPrefData = findViewById(R.id.editText);
+        myPreferenceEditor.putString("key", sharedPrefData.getText().toString());
+        myPreferenceEditor.apply();
     }
 }
